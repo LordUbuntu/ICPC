d5031c35b3f34906748b18243335b66b1dc54df8
Jacobus Burger <therealjacoburger@gmail.com>
2023-01-26 12:59:03 -0800

    improved solution

--- 2014/N.py
+++ 2014/N.py
@@ -1,6 +1,5 @@
 # Jacobus Burger (2022)
 # majority problem, 2014 ICPC D2
-
 def solution():
     T = int(input())
     for _ in range(T):
@@ -16,4 +15,23 @@ def solution():
         for k, v in votes.items():
             if v == max(votes.values()):
                 result.append(k)
-        print(f"{min(result): 8}")
+        print(min(result))
+
+
+# new solution
+# this works because c.most_common returns elements sorted first by
+#   most occurences and then by increasing value, so always taking the
+#   first element of the first index tuple will always return the
+#   least valued most voted number.
+from collections import Counter
+def sol():
+    T = int(input())
+    for _ in range(T):
+        V = int(input())
+        # count occurrences of all votes
+        c = Counter((int(input()) for _ in range(V)))
+        # filter out only the most common numbers
+        max_count = c.most_common()[0][1]
+        c = filter(lambda t: t[1] == max_count, c.most_common())
+        # get smallest number with most votes
+        print(sorted(c)[0][0])
