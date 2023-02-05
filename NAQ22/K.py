83631e87ccd1680cd9c6f64d09a6b68f6c98a95a
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-05 15:29:23 -0800

    fixed solution K - Smallest Calculated Value

--- NAQ22/K.py
+++ NAQ22/K.py
@@ -2,23 +2,25 @@
 # My solution to this problem (very straight-forward)
 # Poblem K - Smallest Caclulated Value
 from itertools import product
-from math import inf
+
 
 a, b, c = map(int, input().split())
-ops = ['+', '-', '*', '//']
-min_val = inf
+ops = ['+', '-', '*', '/']
+min_val = (1000**3)
 
 for op1, op2 in product(ops, ops):
-    if op1 == '//' or op2 == '//':
+    if op1 == '/':
         if a % b != 0:
             continue
-        elif (a // b) % c != 0:
+    val = eval(f"{a} {op1} {b}")
+    if op2 == '/':
+        if val % c != 0:
             continue
-    val = eval(f"({a} {op1} {b}) {op2} {c}")
-    if val < 0:
-        continue
+    val = eval(f"{val} {op2} {c}")
     if val < min_val:
+        if val < 0:
+            continue
         min_val = val
     if min_val == 0:
         break
-print(min_val)
+print(int(min_val))
