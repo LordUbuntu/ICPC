cbe6a5d974271b0d5e9a07a17259a098dfad5e4e
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-06 21:27:00 -0800

    added an improvement

--- NAQ22/C.py
+++ NAQ22/C.py
@@ -1,3 +1,8 @@
 # Problem C - Class Field Trip
 # The wonders of sorted input, we can just cat, sort, print it!
 print(''.join(sorted(list(input() + input()))))
+
+
+# one minor improvement of mine is that the list() conversion is
+#   redundant since a string is treated like a list of char anyways.
+print(''.join(sorted(input() + input())))
