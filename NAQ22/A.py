f097212eb190bbfa715fa344a57876aa1c93fe4d
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-06 21:31:15 -0800

    Added Problem A - Beast Bullies

---
+++ NAQ22/A.py
@@ -0,0 +1,22 @@
+# Problem A - Beast Bullies
+# Explaination:
+#   TODO
+n = int(input())
+animals = sorted([int(input()) for i in range(n)])
+attackers = n-1
+loss = 0
+
+defenceStr = 0
+attackStr = animals[-1]
+
+for i in range(n-2, -1, -1):
+    defenceStr += animals[i]
+    
+    if defenceStr >= attackStr:
+        attackers = i
+        loss = 0
+        attackStr += defenceStr
+        defenceStr = 0
+    else:  
+        loss += 1
+print(n - loss)
