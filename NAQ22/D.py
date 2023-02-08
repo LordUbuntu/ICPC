fb8a5a3e4909679ce0ca4c81627093b3d932e7cc
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-07 21:47:54 -0800

    added problem d - ghost leg solution

---
+++ NAQ22/D.py
@@ -0,0 +1,18 @@
+# Problem D - Ghost Leg
+# Explanation:
+#   TODO
+n,m = input().split()
+n = int(n)
+m = int(m)
+rungs = [int(input()) for _ in range(m)]
+out = [0 for i in range(n)]
+for i in range(1,n+1):
+    pole = i
+    for j in range(0,m):
+        if rungs[j] == pole-1:
+            pole -= 1
+        elif rungs[j] == pole:
+            pole += 1
+    out[pole - 1] = i
+for i in out:
+    print(i)
