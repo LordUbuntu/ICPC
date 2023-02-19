01b6a9d2a22956f2d12d00ef3a9d0b4524f49f49
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-18 18:35:14 -0800

    finished my solution to goat on a rope

--- 2018/T.py
+++ 2018/T.py
@@ -1,6 +1,15 @@
 # goat on a rope
-# Tia's solution (not in the team, but thought it was neat)
+# my solution
+from math import sqrt
 x, y, x1, y1, x2, y2 = map(int, input().split())
-dx = min(abs(x - x1), abs(x - x2))
-dy = min(abs(y - y1), abs(y - y2))
-print(float(max(dx, dy)))
+rx, ry = (x1 + x2)/2, (y1 + y2)/2
+h, w = abs(x1 - x2), abs(y1 - y2)
+dx = max(abs(rx - x) - w/2, 0)
+dy = max(abs(ry - y) - h/2, 0)
+print("%.3f\n" % (round(float(sqrt(dx**2 + dy**2)), 3)))
+
+# Tia's solution (not in the team, but thought it was neat)
+# x, y, x1, y1, x2, y2 = map(int, input().split())
+# dx = min(abs(x - x1), abs(x - x2))
+# dy = min(abs(y - y1), abs(y - y2))
+# print(float(max(dx, dy)))
