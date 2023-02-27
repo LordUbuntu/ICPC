d58a8551d5bdae8263dfd8a133ec840724ee9e42
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-26 21:15:24 -0800

    adding problems, added resturaunt opening

---
+++ 2023/m.py
@@ -0,0 +1,4 @@
+# Resturaunt Opening Problem
+# The fourth problem, solved at the 1h48m mark.
+# Solution:
+#   While we thought be could have solved this more elegantly with maybe a graph algorithm or a dp one, we realized that with the size of input (50), and the worst-case time complexity of a brute force solution (O(n**3)), we decided to just brute force a solution instead, calculating the best value for each location and returning the best one.
