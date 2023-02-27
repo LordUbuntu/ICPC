0c69c6342a4fcad981b3b133be161a5696eeb8eb
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-26 21:14:08 -0800

    adding problems, added fading wind

---
+++ 2023/g.py
@@ -0,0 +1,4 @@
+# Fading Wind Problem
+# One of the first and easiest problems we solved in the contest.
+# Solution:
+#   We solved this problem in the first hour of the contest. It's a straight-forward simulation, we follow the instructions and solve in O(n) time.
