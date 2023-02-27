118e1810ea6d81cafd783216edfc7ae8c1e8f0e5
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-26 21:13:53 -0800

    adding problems, added sun and moon

---
+++ 2023/j.py
@@ -0,0 +1,5 @@
+# Sun and Moon Problem
+# One of the first and easiest problems we solved during the contest.
+# We solved this problem in the first hour of the contest.
+# Solution:
+#   We just calculate in linear time up to when the next expected overlap will be (if there is one). This is O(n), we knew there was a better solution in O(1) but we didn't think we had enough time to find the faster solution, and for the input size of 5000, linear time would be just fine.
