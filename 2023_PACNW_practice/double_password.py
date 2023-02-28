cdd892303ddfbf92a051ccd314f3928854bdf508
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-27 18:59:10 -0800

    shortened solution

--- 2023_PACNW_practice/double_password.py
+++ 2023_PACNW_practice/double_password.py
@@ -1,5 +1,3 @@
 # calculate total number of differences and return 2 to the power of that
 from operator import ne
-s1 = input()
-s2 = input()
-print(2**sum(map(ne, s1, s2)))
+print(2**sum(map(ne, input(), input())))
