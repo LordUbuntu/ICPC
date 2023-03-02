c6437cff3d26745aaaf4500d5488eb2855bd3b55
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-01 22:54:45 -0800

    finished comment

--- 2023/i.py
+++ 2023/i.py
@@ -1,4 +1,4 @@
 # I Could Have Won Problem
 # The third problem, solved at the 1h30m mark.
 # Solution:
-#   For the size of input (2000), we can simply scan across and keep track of game wins and losses to find our answers. So, in O(n) we did just that. We scanned across and counted the wins of alice and bob at each step...
+#   For the size of input (2000), we can simply scan across and keep track of game wins and losses to find our answers. So, in O(n) we did just that. We scanned across and counted the wins of alice and bob at each step, then we use that informaiton to get the correct output.
