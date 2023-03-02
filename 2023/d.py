a2ce7d92fcc77ea6d03759eeb2f33efcf377bbd7
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-01 22:57:01 -0800

    updated comment to give more detail

--- 2023/d.py
+++ 2023/d.py
@@ -1,4 +1,4 @@
 # Hunt the Wumpus Problem
 # The final problem we solved, solved at the 2h14m mark.
 # Solution:
-#   Simple like Fading Wind, TODO
+#   Simple like Fading Wind, we follow the instructions for calculating the pseudorandom locations of each of the four wumpuses, and then we respond to the player input and give output as defined in the problem. We can calculate manhattan of each of the wumpus (there's only 4 max) and return the one with the lowest distance.
